ff52e73b4c134fe45e907df690dc5811c0b86668
Gianluca <gianlucadorelo@gmail.com>
2020-12-08 19:47:38 +0000

    Added task(s)

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 n = number
 if n < 0:
-    n *= -1
-r = n % 10
+    numberpos = n * -1
+else:
+    numberpos = n
+r = numberpos % 10
 if n < 0:
     r *= -1
 if r == 0:
