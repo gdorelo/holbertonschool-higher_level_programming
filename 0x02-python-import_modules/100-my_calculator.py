4f80730af511f21d9cb2f27ed16da8ad9a371c72
Gianluca <gianlucadorelo@gmail.com>
2020-12-10 18:03:38 +0000

    Added Calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv
+    argc = len(argv) - 1
+    if argc != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    if "+" in operator:
+        print("{} {} {} = {}".format(a, s, b, add(a, b)))
+    elif "-" in operator:
+        print("{} {} {} = {}".format(a, s, b, sub(a, b)))
+    elif "*" in operator:
+        print("{} {} {} = {}".format(a, s, b, mul(a, b)))
+    elif "/" in operator:
+        print("{} {} {} = {}".format(a, s, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
