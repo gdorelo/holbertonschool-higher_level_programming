0143b6e95d9b872d286516bb5214a0b15326d5b9
Gianluca <gianlucadorelo@gmail.com>
2020-12-15 19:30:08 +0000

    #100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Python.h>
+/**
+  * print_python_list_info - Print python list info
+  * @p: modulo object
+  * Return: Nothing
+  */
+void print_python_list_info(PyObject *p)
+{
+}
