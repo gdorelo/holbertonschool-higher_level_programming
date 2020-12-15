bee0eb8341d5c7799b7f9b2a2c96105be3abd5e7
Gianluca <gianlucadorelo@gmail.com>
2020-12-15 17:57:18 +0000

    #9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        num = my_list[0]
+        for i in range(len(my_list)):
+            if num < my_list[i]:
+                num = my_list[i]
+        return num
+    else:
+        return None
