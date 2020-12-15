fa973cd317a051107394890b7184d2ed5b609db7
Gianluca <gianlucadorelo@gmail.com>
2020-12-15 17:18:03 +0000

    #5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    list = []
+    list = ""
     for i in my_string:
         if i != 'c' and i != 'C':
-            list.append(i)
+            list += i
     return list
