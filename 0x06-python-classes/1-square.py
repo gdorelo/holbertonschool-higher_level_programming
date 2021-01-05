7013905fa4af1c939ab649224e9289df7ab0076e
Gianluca <gianlucadorelo@gmail.com>
2021-01-05 20:01:24 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''square'''
+
+
+class Square:
+    ''' what are these for? who knows '''
+
+    def __init__(self, size):
+            """Initializing data"""
+            self.__size = size
