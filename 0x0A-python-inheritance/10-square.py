07f58feb73d32e89e9e468aa4d7448b5a45c4829
Atenea <atecast@gmail.com>
2021-01-19 23:51:34 +0000

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Contains the class BaseGeometry"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """what are these for? who knows"""
+    def __init__(self, size):
+        """to initialize"""
+        self.integer_validator("size", size)
+        # ask why:
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """calculate area"""
+        return self.__size * self.__size
