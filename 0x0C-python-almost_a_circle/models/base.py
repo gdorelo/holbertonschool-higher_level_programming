d3f553b2f2e4a7b926e3ccc2caadea4d02690ce5
Gianluca <gianlucadorelo@gmail.com>
2021-01-22 21:39:15 +0000

    added class methods to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 ''' Base class module '''
 
 
+import json
+
+
 class Base:
     ''' Base class '''
     __nb_objects = 0
@@ -14,3 +17,21 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' returns the JSON string representation of list_dictionaries '''
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''' writed the json string representation of list_objs to a file '''
+        filename = cls.__name__ + ".json"
+        list_o = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_o.append(cls.to_dictionary(i))
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_o))
