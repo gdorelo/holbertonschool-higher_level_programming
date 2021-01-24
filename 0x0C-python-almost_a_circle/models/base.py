c61ad07f60a5b851a365992229bf738feec8251a
Gianluca <gianlucadorelo@gmail.com>
2021-01-24 21:49:47 +0000

    Missing a @staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,6 +24,7 @@ class Base:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
 
+    @staticmethod
     def from_json_string(json_string):
         ''' returns the list of the JSON string representation json_string '''
         if json_string is None or len(json_string) == 0:
