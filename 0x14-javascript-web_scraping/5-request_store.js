b63371c48369101f7ce131b8ff7a8b05c7d47c72
Gianluca <gianlucadorelo@gmail.com>
2021-05-04 22:56:18 -0300

    Task 5 again

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,17 @@
 #!/usr/bin/node
+
+// Read the file and print its contents.
+const link = process.argv[2];
+const filename = process.argv[3];
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[1];
-const filePath = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+
+request(link, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filename, body, 'utf8', function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
