a33c158197d752eda99a90c37b6164120d094e22
Gianluca <gianlucadorelo@gmail.com>
2021-05-04 22:42:57 -0300

    Fixed task 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/:id';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+const argv = process.argv;
+const url = `http://swapi.co/api/films/${argv[2]}`;
+
+request(url, function (err, response, body) {
+  if (err) throw err;
+
+  console.log(`${JSON.parse(body).title}`);
 });
