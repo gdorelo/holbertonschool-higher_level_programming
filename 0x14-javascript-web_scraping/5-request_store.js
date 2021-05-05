edc8bb816bd1ed00f0967b6dc61d4cf20cddafd6
Gianluca <gianlucadorelo@gmail.com>
2021-05-04 22:52:45 -0300

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
+const url = process.argv[1];
+const filePath = process.argv[2];
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
