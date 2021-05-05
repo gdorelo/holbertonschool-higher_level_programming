afc9cda55b7d6b41ad513c36ec9836b507b641f4
Gianluca <gianlucadorelo@gmail.com>
2021-05-04 22:43:54 -0300

    Fix Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const argv = process.argv;
-const url = `http://swapi.co/api/films/${argv[2]}`;
+const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`;
 
 request(url, function (err, response, body) {
   if (err) throw err;
