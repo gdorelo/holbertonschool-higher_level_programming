ac475ca2ccc88b852037e14aeb41ced87a2a1d42
Gianluca <gianlucadorelo@gmail.com>
2021-05-12 18:39:50 -0300

    Added tasks and removed unnecesary code

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,3 @@
-$(function () {
-  $('#red_header').click(function () {
-    $('header').css('color', '#FF0000');
-  });
+$('#red_header').click(function () {
+  $(this).css('color', '#FF0000');
 });
